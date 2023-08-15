3542db756a2806fa9080feb33b1401969adf1a3d
hendraaagil <hendraaagil@gmail.com>
2023-08-15 21:45:51 +0700

    chore: add prisma log

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './modules';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableShutdownHooks();
   await app.listen(3000);
 }
 
--- src/services/prisma.service.ts
+++ src/services/prisma.service.ts
@@ -1,15 +1,35 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  async onModuleInit() {
-    await this.$connect();
+export class PrismaService extends PrismaClient<Prisma.PrismaClientOptions, Prisma.LogLevel> implements OnModuleInit {
+  private readonly logger = new Logger(PrismaService.name);
+
+  constructor() {
+    super({
+      log: [
+        { emit: 'event', level: 'query' },
+        { emit: 'event', level: 'error' },
+        { emit: 'event', level: 'info' },
+        { emit: 'event', level: 'warn' },
+      ],
+    });
   }
 
-  async enableShutdownHooks(app: INestApplication) {
-    this.$on('beforeExit', async () => {
-      await app.close();
+  async onModuleInit() {
+    await this.$connect();
+
+    this.$on('error', ({ message }) => {
+      this.logger.error(message);
+    });
+    this.$on('warn', ({ message }) => {
+      this.logger.warn(message);
+    });
+    this.$on('info', ({ message }) => {
+      this.logger.debug(message);
+    });
+    this.$on('query', ({ query, params }) => {
+      this.logger.log(`${query}; ${params}`);
     });
   }
 }
